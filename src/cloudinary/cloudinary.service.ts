271a662ea86027a838c656e60fdfc129b4b9919c
Nazmul Islam <nazmuul420@gmail.com>
2021-09-30 12:08:34 +0600

    SingleImageUploadInterceptor refectored!

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -10,10 +10,16 @@ export class CloudinaryService {
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     const folder = `Shop/${folderName}`;
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream({ folder }, (error, result) => {
-        if (error) return reject(error);
-        if (result) return resolve(result);
-      });
+      const upload = v2.uploader.upload_stream(
+        {
+          folder,
+          quality: 50,
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          if (result) return resolve(result);
+        },
+      );
       toStream(file.buffer).pipe(upload);
     });
   }
