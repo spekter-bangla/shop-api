3044dbed194520b6339771ce42196feb05e40e12
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-10-08 15:43:20 +0600

    order item get all and single order item endpoint is added

--- src/orders-items/orders-items.controller.ts
+++ src/orders-items/orders-items.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import { CloudinaryService } from "src/cloudinary/cloudinary.service";
+import { OrdersItemsService } from "./orders-items.service";
 
-@Controller('orders-items')
-export class OrdersItemsController {}
+@Controller("orders-items")
+export class OrdersItemsController {
+  constructor(
+    private readonly orderItemsService: OrdersItemsService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
+
+  @Get("/")
+  async getAllOrderItem() {
+    const orderitemAll = await this.orderItemsService.findAllOrderItem();
+    return {
+      status: "Success",
+      data: orderitemAll,
+    };
+  }
+
+  @Get("/:id")
+  async getSingleOrderItem(@Param("id") id: string) {
+    const orderItem = await this.orderItemsService.findSingleOrderItem(id);
+    return {
+      status: "Success",
+      data: orderItem,
+    };
+  }
+}
--- src/orders-items/orders-items.module.ts
+++ src/orders-items/orders-items.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { OrdersItemsController } from './orders-items.controller';
-import { OrdersItemsService } from './orders-items.service';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CloudinaryModule } from "src/cloudinary/cloudinary.module";
+import { OrderItemSchema } from "./order-item.model";
+import { OrdersItemsController } from "./orders-items.controller";
+import { OrdersItemsService } from "./orders-items.service";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: "OrderItem", schema: OrderItemSchema }]),
+    CloudinaryModule,
+  ],
   controllers: [OrdersItemsController],
-  providers: [OrdersItemsService]
+  providers: [OrdersItemsService],
+  exports: [OrdersItemsService],
 })
 export class OrdersItemsModule {}
